8ade6fabbde4c13b710e2c6fd0fb6ea9ca5518e3
Adi <119108579+Adityaxyz1@users.noreply.github.com>
2026-04-18 22:11:02 +0530

    Delete HireSense/list_models.py

--- HireSense/list_models.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDG_Kc8ooz6ej43HeACZcrj9n_zKgDILYg")
-models = genai.list_models()
-for m in models:
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
